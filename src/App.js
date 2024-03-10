c353d9597cd286522e64ea7a0eaaf3a2551c51cb
rostisla88 <kakashkaqq123@gmail.com>
2024-03-10 18:29:38 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import GameStart from './views/GameStart';
 
 function App() {
+
+  const tg =  window.Telegram.WebApp;
+
+const onClose = () =>{
+  tg.close()
+}
+
+
   return (
     <div className="App">
+      <button onClick={onClose}>Close app</button>
       <GameStart/>
     </div>
   );
